1f53d775ac7286301b482366c4bc1059e954c189
Owen <181250092@smail.nju.edu.cn>
2020-05-29 00:14:38 +0800

    feature/userinterface-release

--- src/views/UserModule/Settings.vue
+++ src/views/UserModule/Settings.vue
@@ -1,14 +1,52 @@
 <template>
 <div>
-    <avatar :username="username" style="margin-left: 47%; margin-bottom: 40px" color="rgb(255, 255, 255)"></avatar>
+    <avatar :username="basic.username" style="margin-left: 47%; margin-bottom: 40px" color="rgb(255, 255, 255)"></avatar>
     <!-- <a-icon type="user" style="font-size: 50px; margin-left: 47% ;margin-bottom: 20px"/> -->
-    <h1 class="h1display">Username : <span class="inlineattribute">{{username}}</span></h1>
-    <h1 class="h1display">Gender : <span class="inlineattribute">{{sex}}</span></h1>
-    <h1 class="h1display">Age : <span class="inlineattribute">{{age}}</span></h1>
-    <h1 class="h1display">Password : <span style="display: inline-block; margin-bottom: 50px">{{password}}</span></h1>
+    <h1 class="h1display">Username : 
+        <span v-if="state == 'show'" class="inlineattribute">{{basic.username}}</span>
+        <span><a-input 
+                v-if="state == 'modify'" 
+                :placeholder="username" 
+                style="background: lightgray; width: 30%" 
+                allow-clear 
+                class="inlineattribute" 
+                v-model="basic.username"/></span>
+    </h1>
+    <h1 class="h1display">Gender : 
+        <span v-if="state == 'show'" class="inlineattribute">{{basic.sex}}</span>
+        <span>
+            <a-select default-value="Male" v-if="state == 'modify'" style="width: 20%; display: inline-block; margin-left: 42px;" @change="handleChange">
+                <a-select-option value="Male">Male</a-select-option>
+                <a-select-option value="Female">Female</a-select-option>
+            </a-select>
+        </span>
+    </h1>
+    <h1 class="h1display">Age : 
+        <span v-if="state == 'show'" class="inlineattribute">{{basic.age}}</span>
+        <span>
+            <a-input 
+            v-if="state == 'modify'" 
+            :placeholder="basic.age" 
+            style="background: lightgray; width:20%; display: inline-block; margin-left: 68px;" 
+            allow-clear 
+            v-model="basic.age"/>
+        </span>
+    </h1>
+    <h1 class="h1display">Password : 
+        <span v-if="state == 'show'" style="display: inline-block">{{basic.password}}</span>
+        <span>
+            <a-input 
+            v-if="state == 'modify'" 
+            :placeholder="basic.password" 
+            style="background: lightgray; width: 30%; display: inline-block; margin-left: 27px;" 
+            allow-clear 
+            v-model="basic.password"/>
+        </span>
+    </h1>
     <div>
-        <a-button type="danger" style="margin-left: 35%">Modify</a-button>
-        <a-button type="primary" style="margin-left: 10%">Confirm</a-button>
+        <a-button v-if="state == 'show'" type="danger" style="margin-left: 45%" @click="stateModify">Modify</a-button>
+        <a-button v-if="state == 'modify'" style="margin-left: 35%; margin-top: 20px" @click="stateCancel">Cancel</a-button>
+        <a-button v-if="state == 'modify'" type="primary" style="margin-left: 10%" @click="stateConfirm">Confirm</a-button>
     </div>
 </div>
 </template>
@@ -20,10 +58,21 @@ export default {
     name : 'Settings',
     data(){
         return{
-            username: '',
-            sex: 'Male',
-            age: 0,
-            password: '',
+            basic: {
+                userid: null,
+                username: '',
+                sex: 'Male',
+                age: 0,
+                password: '',
+            },
+            state: 'show',
+            duplicate:{
+                userid: null,
+                username: '',
+                sex: 'Male',
+                age: 0,
+                password: '',
+            },
         }
     },
     components:{
@@ -32,26 +81,73 @@ export default {
     mounted(){
         this.constructor()  // 获取账户的资料
     },
+    actions(){
+    },
     methods: {
         constructor(){
             let userList
             axios.get('http://localhost:3000/user/getuserlist').then((res) => {
                 console.log("Excute the constructor function!")
               userList = res.data.users.users
-              this.username = this.$store.getters.getUsername
-              console.log(this.username)
+              this.basic.username = this.$store.getters.getUsername
+              console.log(this.basic.username)
               for (var user of userList){
-                if (user.username === this.username){
-                    this.password = user.password
+                if (user.username == this.basic.username){
+                    this.basic.password = user.password
+                    this.basic.userid = user.userid
                     if (user.sex != null){
-                        this.sex = user.sex
+                        this.basic.sex = user.sex
                     }
                     if (user.age != null){
-                        this.age = user.age
+                        this.basic.age = user.age
                     }
                 }
               }
             })
+        },
+        stateConfirm(){
+            if (this.basic.age < 0 || this.basic.age > 120){
+                this.$message.warning('Age is not allowed.', 3);
+                this.stateCancel()
+            }else{
+                this.handleUsernameCheck()
+            }
+            
+        },
+        stateModify(){
+            const data = this.basic
+            this.duplicate = data
+            this.state = 'modify'
+        },
+        stateCancel(){
+            this.basic = this.duplicate
+            this.state = 'show'
+        },
+        handleChange(value){
+            this.basic.sex = value
+        },
+        handleUsernameCheck(){
+            let userList
+            axios.get('http://localhost:3000/user/getuserlist').then((res) => {
+            userList = res.data.users.users
+            for (var user of userList){
+                if (user.username == this.basic.username){
+                    console.log("Execute false")
+                    return Promise.reject()
+                }
+            }
+                this.$message.success('Modify successfully !', 3)
+                console.log(this.basic.username)
+                this.changeUser()
+                this.state = 'show'
+            }).catch(() => {
+                this.$message.warning('Username is used.', 3);
+                this.stateCancel()
+            })
+        },
+        changeUser(){
+            axios.post('http://localhost:3000/user/updateuser', this.basic)
+            this.$store.commit('setUsername', this.basic.username)
         },
     },
 }
